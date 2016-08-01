c4d0e9c9cd19e3381c616eb972551ac7c4416819
Yuan Liu <liuyuanneu@gmail.com>
2016-08-01 11:09:11 -0400

    Add UserAvatar

--- client/compiled/src/bundle.js
+++ client/compiled/src/bundle.js
@@ -71,7 +71,7 @@
 
 	var _router2 = _interopRequireDefault(_router);
 
-	var _combine = __webpack_require__(278);
+	var _combine = __webpack_require__(280);
 
 	var _combine2 = _interopRequireDefault(_combine);
 
@@ -27037,11 +27037,11 @@
 
 	var _dashboard2 = _interopRequireDefault(_dashboard);
 
-	var _signupForm = __webpack_require__(274);
+	var _signupForm = __webpack_require__(276);
 
 	var _signupForm2 = _interopRequireDefault(_signupForm);
 
-	var _loginForm = __webpack_require__(276);
+	var _loginForm = __webpack_require__(278);
 
 	var _loginForm2 = _interopRequireDefault(_loginForm);
 
@@ -48387,15 +48387,19 @@
 
 	var _redux = __webpack_require__(167);
 
+	var _reactUseravatar = __webpack_require__(272);
+
+	var _reactUseravatar2 = _interopRequireDefault(_reactUseravatar);
+
 	var _actionCreator = __webpack_require__(258);
 
 	var _auth = __webpack_require__(250);
 
-	var _flash = __webpack_require__(272);
+	var _flash = __webpack_require__(274);
 
 	var _flash2 = _interopRequireDefault(_flash);
 
-	var _dashboardDataInput = __webpack_require__(273);
+	var _dashboardDataInput = __webpack_require__(275);
 
 	var _dashboardDataInput2 = _interopRequireDefault(_dashboardDataInput);
 
@@ -48410,7 +48414,6 @@
 	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } // Import all required modules
-
 
 	// Import all actions & helper methods
 
@@ -48607,6 +48610,11 @@
 	                'div',
 	                null,
 	                _react2.default.createElement(
+	                  'div',
+	                  { className: 'avatar' },
+	                  _react2.default.createElement(_reactUseravatar2.default, { username: this.props.userInfo.name })
+	                ),
+	                _react2.default.createElement(
 	                  'p',
 	                  { className: 'lead' },
 	                  'Name: ',
@@ -48689,6 +48697,236 @@
 /* 272 */
 /***/ function(module, exports, __webpack_require__) {
 
+	/**
+	 * Created by Nicolas Bellot @bltnico
+	 * Links : https://github.com/bltnico
+	 */
+
+	var UserAvatar = __webpack_require__(273);
+	module.exports = UserAvatar;
+
+
+/***/ },
+/* 273 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _react = __webpack_require__(2);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	/**
+	 * @class UserAvatar
+	 * @extends React.Component
+	 *
+	 * @author https://github.com/bltnico
+	 * @url https://github.com/bltnico/react-user-avatar
+	 * @version 1.1.1
+	 *
+	 * @example
+	 *
+	 * <UserAvatar
+	 *     size={100}
+	 *     onClick={this.doSomething}
+	 *     username="bltnico"
+	 *     textColor="#ffffff" />
+	 *
+	 */
+
+	var UserAvatar = function (_React$Component) {
+	    _inherits(UserAvatar, _React$Component);
+
+	    /**
+	     * Constructor
+	     *
+	     * @param {Object} props
+	     */
+
+	    function UserAvatar(props) {
+	        _classCallCheck(this, UserAvatar);
+
+	        var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(UserAvatar).call(this, props));
+
+	        _this.state = {
+	            color: null,
+	            text: null
+	        };
+	        return _this;
+	    }
+
+	    _createClass(UserAvatar, [{
+	        key: 'componentDidMount',
+	        value: function componentDidMount() {
+	            this.setState({
+	                color: this.getColor(),
+	                text: this.getText()
+	            });
+	        }
+
+	        /**
+	         * @static getColor
+	         * @param {String} str
+	         * @return {String}
+	         */
+
+	    }, {
+	        key: 'hashCode',
+
+	        /**
+	         * Thanks to Cristian Sanchez
+	         * @url http://stackoverflow.com/a/3426956/4185200
+	         *
+	         * @param {String} str
+	         * @return {String}
+	         */
+	        value: function hashCode(str) {
+	            var hash = 0,
+	                i = 0;
+	            for (i; i < str.length; i++) {
+	                hash = str.charCodeAt(i) + ((hash << 5) - hash);
+	            }return hash;
+	        }
+
+	        /**
+	         * Thanks to Cristian Sanchez
+	         * @url http://stackoverflow.com/a/3426956/4185200
+	         *
+	         * @param {Integer} int
+	         * @return {String}
+	         */
+
+	    }, {
+	        key: 'intToRGB',
+	        value: function intToRGB(i) {
+	            var c = (i & 0x00FFFFFF).toString(16).toUpperCase();
+
+	            return "00000".substring(0, 6 - c.length) + c;
+	        }
+
+	        /**
+	         * Get color with username
+	         * @return {String}
+	         */
+
+	    }, {
+	        key: 'getColor',
+	        value: function getColor() {
+	            var hash = this.hashCode(this.props.username);
+	            return this.intToRGB(hash);
+	        }
+
+	        /**
+	         * Get text with username
+	         * @return {String}
+	         */
+
+	    }, {
+	        key: 'getText',
+	        value: function getText() {
+	            var f = undefined,
+	                l = undefined;
+	            f = this.props.username.substr(0, 1);
+	            l = this.props.username.substr(this.props.username.length - 1, this.props.username.length);
+	            return (f + l).toUpperCase();
+	        }
+
+	        /**
+	         * Event when user click on user avatar
+	         * @return {Function}
+	         */
+
+	    }, {
+	        key: '_onClick',
+	        value: function _onClick() {
+	            return this.props.onClick();
+	        }
+
+	        /**
+	         * Render UserAvatar component
+	         * @return {String}
+	         */
+
+	    }, {
+	        key: 'render',
+	        value: function render() {
+
+	            var styles = {
+	                container: {
+	                    backgroundColor: !this.props.borderStyle ? "#" + this.state.color : "transparent",
+	                    border: !this.props.borderStyle ? "none" : "2px solid " + this.state.color,
+	                    borderRadius: this.props.size / 2,
+	                    width: this.props.size,
+	                    height: this.props.size,
+	                    textAlign: 'center',
+	                    lineHeight: this.props.size + "px"
+	                },
+	                text: {
+	                    color: !this.props.borderStyle ? this.props.textColor : this.state.color,
+	                    fontSize: this.props.size / 3 + "px"
+	                }
+	            };
+
+	            return _react2.default.createElement(
+	                'div',
+	                { style: styles.container, onClick: this._onClick.bind(this) },
+	                _react2.default.createElement(
+	                    'span',
+	                    { style: styles.text },
+	                    this.state.text
+	                )
+	            );
+	        }
+	    }], [{
+	        key: 'getColor',
+	        value: function getColor(str) {
+	            return this.intToRGB(this.hashCode(str));
+	        }
+	    }]);
+
+	    return UserAvatar;
+	}(_react2.default.Component);
+
+	/**
+	 * Define UserAvatar propTypes
+	 */
+
+	exports.default = UserAvatar;
+	UserAvatar.propTypes = {
+	    username: _react2.default.PropTypes.string.isRequired,
+	    size: _react2.default.PropTypes.number,
+	    onClick: _react2.default.PropTypes.func,
+	    textColor: _react2.default.PropTypes.string,
+	    borderStyle: _react2.default.PropTypes.bool
+	};
+
+	/**
+	 * Init default UserAvatar props
+	 */
+	UserAvatar.defaultProps = {
+	    size: 100,
+	    textColor: "#ffffff",
+	    borderStyle: false
+	};
+
+/***/ },
+/* 274 */
+/***/ function(module, exports, __webpack_require__) {
+
 	"use strict";
 
 	Object.defineProperty(exports, "__esModule", {
@@ -48744,7 +48982,7 @@
 	exports.default = Flash;
 
 /***/ },
-/* 273 */
+/* 275 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -48891,7 +49129,7 @@
 	exports.default = DataInput;
 
 /***/ },
-/* 274 */
+/* 276 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -48918,7 +49156,7 @@
 
 	var _actionCreator = __webpack_require__(258);
 
-	var _signupInput = __webpack_require__(275);
+	var _signupInput = __webpack_require__(277);
 
 	var _signupInput2 = _interopRequireDefault(_signupInput);
 
@@ -49046,7 +49284,7 @@
 	exports.default = (0, _reactRedux.connect)(mapStateToProps, mapDispatchToProps)(SignForm);
 
 /***/ },
-/* 275 */
+/* 277 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -49188,7 +49426,7 @@
 	exports.default = SignupInput;
 
 /***/ },
-/* 276 */
+/* 278 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -49215,11 +49453,11 @@
 
 	var _actionCreator = __webpack_require__(258);
 
-	var _flash = __webpack_require__(272);
+	var _flash = __webpack_require__(274);
 
 	var _flash2 = _interopRequireDefault(_flash);
 
-	var _loginInput = __webpack_require__(277);
+	var _loginInput = __webpack_require__(279);
 
 	var _loginInput2 = _interopRequireDefault(_loginInput);
 
@@ -49379,7 +49617,7 @@
 	exports.default = (0, _reactRedux.connect)(mapStateToProps, mapDispatchToProps)(LoginForm);
 
 /***/ },
-/* 277 */
+/* 279 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -49496,7 +49734,7 @@
 	exports.default = LoginInput;
 
 /***/ },
-/* 278 */
+/* 280 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -49507,15 +49745,15 @@
 
 	var _redux = __webpack_require__(167);
 
-	var _searchReducer = __webpack_require__(279);
+	var _searchReducer = __webpack_require__(281);
 
 	var _searchReducer2 = _interopRequireDefault(_searchReducer);
 
-	var _userInfoReducer = __webpack_require__(280);
+	var _userInfoReducer = __webpack_require__(282);
 
 	var _userInfoReducer2 = _interopRequireDefault(_userInfoReducer);
 
-	var _cityStateReducer = __webpack_require__(281);
+	var _cityStateReducer = __webpack_require__(283);
 
 	var _cityStateReducer2 = _interopRequireDefault(_cityStateReducer);
 
@@ -49534,7 +49772,7 @@
 	exports.default = rootReducer;
 
 /***/ },
-/* 279 */
+/* 281 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -49557,7 +49795,7 @@
 	var _actionCreator = __webpack_require__(258);
 
 /***/ },
-/* 280 */
+/* 282 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -49580,7 +49818,7 @@
 	var _actionCreator = __webpack_require__(258);
 
 /***/ },
-/* 281 */
+/* 283 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
