3f7c20dd58a123b28afe0e764ed208a43e5bda18
Jason Bland <jtbland@gmail.com>
2016-08-01 08:29:16 -0700

    (feat) Add basic tagcloud/stackcloud functionality

--- client/src/components/keywordCloud/cloud.jsx
+++ client/src/components/keywordCloud/cloud.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { TagCloud, defaultRenderer } from "react-tagcloud";
 
-const Cloud = React.createClass({
-  render: function() {
-    return (
-      <div>
+const data = [
+  { value: "Angular", count: 38 },
+  { value: "React", count: 30 },
+  { value: "Nodejs", count: 28 },
+  { value: "Express.js", count: 25 },
+  { value: "HTML5", count: 33 },
+  { value: "CSS3", count: 33 },
+  { value: "MongoDB", count: 18 }
+];
 
-      </div>
-    );
-  }
-});
+const Cloud = () => (
+  <Cloud minSize={12}
+            maxSize={35}
+            tags={data}
+            onClick={tag => console.log('clicking on tag:', tag)} />
+);
 
 export default Cloud;
