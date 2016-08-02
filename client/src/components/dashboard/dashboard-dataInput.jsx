9a8f156beb4f40af1b6394072664233623c0d820
Jason Bland <jtbland@gmail.com>
2016-08-01 17:42:54 -0700

    (feat) Add state validation to datainput

--- client/src/components/dashboard/dashboard-dataInput.jsx
+++ client/src/components/dashboard/dashboard-dataInput.jsx
@@ -15,23 +15,80 @@ var DataInput = (props) => (
         <div className="col-sm-1">
             <span className="glyphicon glyphicon-globe"></span>
         </div>
-
         <div className="col-sm-11">
-          <input type="text" value={props.city} className="form-control" onChange={props.addCity} placeholder="Add your city" />
+          <input
+            type="text"
+            value={props.city}
+            className="form-control"
+            onChange={props.addCity}
+            placeholder="Add your city"
+          />
         </div>
-
       </fieldset>
 
       <fieldset className="form-group row gray">
         <div className="col-sm-1">
-            <span className="glyphicon glyphicon-globe"></span>
+          <span className="glyphicon glyphicon-globe"></span>
         </div>
-
         <div className="col-sm-11">
-          <input type="text" value={props.state} className="form-control" onChange={props.addState} placeholder="Add your state" />
-
+          <select
+          className="center-block form-control fit"
+          value={props.state}
+          onChange={props.addState}
+          >
+            <option disabled selected>Add your desired state</option>
+            <option value='AL'>AL</option>
+            <option value='AK'>AK</option>
+            <option value='AZ'>AZ</option>
+            <option value='AR'>AR</option>
+            <option value='CA'>CA</option>
+            <option value='CO'>CO</option>
+            <option value='CT'>CT</option>
+            <option value='DE'>DE</option>
+            <option value='FL'>FL</option>
+            <option value='GA'>GA</option>
+            <option value='HI'>HI</option>
+            <option value='ID'>ID</option>
+            <option value='IL'>IL</option>
+            <option value='IN'>IN</option>
+            <option value='IA'>IA</option>
+            <option value='KS'>KS</option>
+            <option value='KY'>KY</option>
+            <option value='LA'>LA</option>
+            <option value='ME'>ME</option>
+            <option value='MD'>MD</option>
+            <option value='MA'>MA</option>
+            <option value='MI'>MI</option>
+            <option value='MN'>MN</option>
+            <option value='MS'>MS</option>
+            <option value='MO'>MO</option>
+            <option value='MT'>MT</option>
+            <option value='NE'>NE</option>
+            <option value='NV'>NV</option>
+            <option value='NH'>NH</option>
+            <option value='NJ'>NJ</option>
+            <option value='NM'>NM</option>
+            <option value='NY'>NY</option>
+            <option value='NC'>NC</option>
+            <option value='ND'>ND</option>
+            <option value='OH'>OH</option>
+            <option value='OK'>OK</option>
+            <option value='OR'>OR</option>
+            <option value='PA'>PA</option>
+            <option value='RI'>RI</option>
+            <option value='SC'>SC</option>
+            <option value='SD'>SD</option>
+            <option value='TN'>TN</option>
+            <option value='TX'>TX</option>
+            <option value='UT'>UT</option>
+            <option value='VT'>VT</option>
+            <option value='VA'>VA</option>
+            <option value='WA'>WA</option>
+            <option value='WV'>WV</option>
+            <option value='WI'>WI</option>
+            <option value='WY'>WY</option>
+          </select>
         </div>
-
       </fieldset>
 
 
@@ -39,12 +96,15 @@ var DataInput = (props) => (
         <div className="col-sm-1">
             <span className="glyphicon glyphicon-education"></span>
         </div>
-
         <div className="col-sm-11">
-          <input type="text" value={props.education} className="form-control" onChange={props.addEducation} placeholder="Add your education level" />
-
+          <input
+            type="text"
+            value={props.education}
+            className="form-control"
+            onChange={props.addEducation}
+            placeholder="Add your education level"
+          />
         </div>
-
       </fieldset>
 
 
@@ -52,44 +112,59 @@ var DataInput = (props) => (
         <div className="col-sm-1">
             <span className="glyphicon glyphicon-briefcase"></span>
         </div>
-
         <div className="col-sm-11">
-          <input type="text" value={props.experience} className="form-control" onChange={props.addExperience} placeholder="Years of experience" />
+          <input
+            type="text"
+            value={props.experience}
+            className="form-control"
+            onChange={props.addExperience}
+            placeholder="Years of experience"
+          />
         </div>
-
       </fieldset>
 
-       <fieldset className="form-group row gray">
+      <fieldset className="form-group row gray">
         <div className="col-sm-1">
             <span className="glyphicon glyphicon-list-alt"></span>
         </div>
-
         <div className="col-sm-11">
-          <input type="text" value={props.position} className="form-control" onChange={props.addPosition} placeholder="Position title" />
-
+          <input
+            type="text"
+            value={props.position}
+            className="form-control"
+            onChange={props.addPosition}
+            placeholder="Position title"
+          />
         </div>
-
       </fieldset>
 
       <fieldset className="form-group row gray">
         <div className="col-sm-1">
             <span className="glyphicon glyphicon-usd"></span>
         </div>
-
         <div className="col-sm-11">
-          <input type="text" value={props.salary} className="form-control" onChange={props.addSalary} placeholder="Salary" />
-
+          <input
+            type="text"
+            value={props.salary}
+            className="form-control"
+            onChange={props.addSalary}
+            placeholder="Salary"
+          />
         </div>
-
       </fieldset>
 
       <fieldset className="form-group row gray">
         <div className="col-sm-1">
             <span className="glyphicon glyphicon-equalizer"></span>
         </div>
-
         <div className="col-sm-11">
-          <input type="text" value={props.stack} className="form-control" onChange={props.addStack} placeholder="Add any stack skills separated by commas" />
+          <input
+            type="text"
+            value={props.stack}
+            className="form-control"
+            onChange={props.addStack}
+            placeholder="Add any stack skills separated by commas"
+          />
         </div>
       </fieldset>
 
